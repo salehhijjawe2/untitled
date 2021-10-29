113bf0847761b4f80958adb2711c4ed650d5075b
Saleh Abuali <salehabuaous@gmail.com>
2021-10-29 20:45:15 +0200

    Initial commit

---
+++ src/class2.java
@@ -0,0 +1,5 @@
+public class class2 {
+    class2(int i,int j){
+        System.out.println(i+j);
+    }
+}
---
+++ src/testClass.java
@@ -0,0 +1,5 @@
+public class testClass {
+    public static void main(String[] args) {
+        class2 b=new class2(5,3);
+    }
+}
